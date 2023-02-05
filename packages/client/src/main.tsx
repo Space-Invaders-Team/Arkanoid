230f87a854ca2e6230d4afa127776f29db9dc22b
Julia Dmitrieva <pennycat@yandex.ru>
2023-02-05 20:19:54 +0300

    style: replace styles and fonts folders

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/reset.css';
-import './styles/fonts.css';
-import './styles/colors.css';
-import './styles/index.css';
-import { LeaderBoard } from './pages/Leaderboard/Leaderboard';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <LeaderBoard />
   </React.StrictMode>,
 );
