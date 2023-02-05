fa9f639931269ee282734d3456bcfd15c650aa6d
Julia Dmitrieva <pennycat@yandex.ru>
2023-02-05 04:00:37 +0300

    style: add theme colors and fonts

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/reset.css';
+import './styles/fonts.css';
+import './styles/colors.css';
+import './styles/index.css';
+import { LeaderBoard } from './pages/Leaderboard/Leaderboard';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <LeaderBoard />
   </React.StrictMode>,
 );
