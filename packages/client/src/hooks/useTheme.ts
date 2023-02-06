6fdcb9f2f65bf452d979a63465c4158bfbcc0e6b
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2023-02-07 00:45:30 +0100

    Jest-tests Fix

--- packages/client/src/hooks/useTheme.ts
+++ packages/client/src/hooks/useTheme.ts
@@ -1,5 +1,21 @@
 import { useLayoutEffect, useState } from 'react';
 
+// defineProperty for jest-test
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches;
 const defaultTheme = isDarkTheme ? 'dark' : 'light';
 
