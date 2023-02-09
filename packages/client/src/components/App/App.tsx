a7cfbed5f2ab4a693414249878d3f95251a3b174
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2023-02-09 16:26:14 +0100

    Temporary stash code of App.

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -1,19 +1,19 @@
-import { StrictMode, useEffect } from 'react';
+import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Navbar } from '../Navbar/navbar';
 import Router from '../../router/router';
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-    };
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`;
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     console.log(data);
+  //   };
 
-    fetchServerData();
-  }, []);
+  //   fetchServerData();
+  // }, []);
 
   return (
     <StrictMode>
