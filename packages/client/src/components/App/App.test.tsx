b7453d70f9039ab7259a58284d9c219c7d5eab8b
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2023-02-09 16:39:28 +0100

    Unstash code for theme.

--- packages/client/src/components/App/App.test.tsx
+++ packages/client/src/components/App/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 // defineProperty for jest-test
 // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-// import './matchMedia.mock'; // Must be imported before the tested file
+import './matchMedia.mock'; // Must be imported before the tested file
 import App from './App';
 
 const appContent = 'Главная';
