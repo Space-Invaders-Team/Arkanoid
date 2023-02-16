c586393aa87802560451a002480edc287be38044
Karpov Yuri <yurick@bk.ru>
2023-02-17 01:31:09 +0300

    fix: Очередная попытка переименовать файлы

--- packages/client/src/pages/authorization/authorization.tsx
+++ packages/client/src/pages/authorization/authorization.tsx
@@ -1,5 +1,5 @@
 import styles from './Authorization.module.css';
-import { Form } from '../../components/Form/Form';
+import { Form } from '../../components/Form2/Form';
 
 export function Authorization() {
   return (
