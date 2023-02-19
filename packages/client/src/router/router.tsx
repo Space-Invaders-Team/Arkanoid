0b2ea9579fb246ef7dcbc4895ff49d632d7e30e8
EstrellaPolaris <mshcherbinina73@gmail.com>
2023-02-19 17:01:09 +0100

    added profile template

--- packages/client/src/router/router.tsx
+++ packages/client/src/router/router.tsx
@@ -3,6 +3,7 @@ import { Authorization } from '../pages/Authorization';
 import { Forum } from '../pages/forum';
 import { TopicList } from '../pages/forum/topicList';
 import { Landing } from '../pages/landing';
+import { Profile } from '../pages/Profile';
 import { Leaderboard } from '../pages/leaderboard';
 import { Registration } from '../pages/Registration';
 import { GamePage } from '../pages/GamePage';
@@ -17,6 +18,7 @@ export function Router() {
         <Route path="/forum/topicList/:id" element={<TopicList />} />
       </Route>
       <Route path="/game" element={<GamePage />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="/leaderboard" element={<Leaderboard />} />
       <Route path="/registration" element={<Registration />} />
     </Routes>
