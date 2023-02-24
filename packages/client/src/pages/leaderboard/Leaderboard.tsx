da29098ff645e60411cd164307dfc09ea2a4d961
Julia Dmitrieva <pennycat@yandex.ru>
2023-02-25 01:02:23 +0300

    Task/25 add Leaderboard API, add Loader component, linter fixes

--- packages/client/src/pages/leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/leaderboard/Leaderboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { leaderboardAPI } from '../../api/LeaderboardAPI/LeaderboardAPI';
 import { TableHead } from '../../components/Leaderboard/TableHead';
 import { TableRow } from '../../components/Leaderboard/TableRow';
@@ -7,77 +7,16 @@ import styles from './Leaderboard.module.css';
 import { Order, TLeaderBoard } from './typings';
 
 export function Leaderboard() {
-  const [leaders, setLeaders] = useState<TLeaderBoard[]>([
-    {
-      id: 123,
-      name: 'Безумный Майк',
-      score: 999999,
-      place: 1,
-    },
-    {
-      id: 1234,
-      name: 'Вася Петров',
-      score: 44234,
-      place: 2,
-    },
-    {
-      id: 12345,
-      name: 'Аня Иванов',
-      score: 23322,
-      place: 3,
-    },
-    {
-      id: 123456,
-      name: 'Владимир Ильич',
-      score: 20000,
-      place: 4,
-    },
-    {
-      id: 3213,
-      name: 'Кот',
-      avatar: '/avatar.jpg',
-      score: 12,
-      place: 5,
-    },
-    {
-      id: 12123,
-      name: 'Кот',
-      avatar: '/avatar.jpg',
-      score: 12,
-      place: 6,
-    },
-    {
-      id: 32,
-      name: 'Кот',
-      avatar: '/avatar.jpg',
-      score: 12,
-      place: 7,
-    },
-    {
-      id: 1233,
-      name: 'Кот',
-      avatar: '/avatar.jpg',
-      score: 12,
-      place: 8,
-    },
-    {
-      id: 33,
-      name: 'Кот',
-      avatar: '/avatar.jpg',
-      score: 12,
-      place: 9,
-    },
-  ]);
+  const [leaders, setLeaders] = useState<TLeaderBoard[]>([]);
   const [loading, setLoading] = useState(true);
 
   function fetchLeaders() {
-    const promise = leaderboardAPI.getAllLiders({
+    leaderboardAPI.getAllLiders({
       ratingFieldName: 'score',
       cursor: 0,
       limit: 15,
-    });
+    })
 
-    promise
       .then((result) => {
         setLeaders(result.map((item: Record<string, TLeaderBoard>, index: number) => {
           item.data.place = index + 1;
