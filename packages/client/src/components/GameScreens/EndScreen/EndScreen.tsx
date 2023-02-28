08c2bf9c1f8dd594edd89966a354d88bed4e5ffe
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2023-02-28 16:08:16 +0300

    feat(task-66): add game levels

---
+++ packages/client/src/components/GameScreens/EndScreen/EndScreen.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import { Button } from '../../Button';
+import { GameStatus } from '../../../pages/GamePage/typings';
+import commonStyles from '../GameScreens.module.css';
+import styles from './EndScreen.module.css';
+import type { Props } from './typings';
+
+const titleMap = new Map<GameStatus, string>([
+  [GameStatus.WIN, 'FLAWLESS VICTORY'],
+  [GameStatus.LOSE, 'GAME OVER'],
+]);
+
+const videoMap = new Map<GameStatus, string>([
+  [GameStatus.WIN, '/win.mp4'],
+  [GameStatus.LOSE, '/lose.mp4'],
+]);
+
+export function EndScreen({ status, onClickPrimaryBtn }: Props) {
+  const endScreenClassName = classNames(commonStyles.dummyScreen, styles.container);
+  const endButtonClassName = classNames(commonStyles.button);
+
+  return (
+    <div className={endScreenClassName}>
+      <span className={styles.text}>{titleMap.get(status)}</span>
+      <video className={styles.video} autoPlay loop muted src={videoMap.get(status)} />
+      <Button
+        extraClassName={endButtonClassName}
+        onClick={onClickPrimaryBtn}
+      >
+        Начать заново
+      </Button>
+    </div>
+  );
+}
--- packages/client/src/pages/GamePage/artifacts/BricksContainer.ts
+++ packages/client/src/pages/GamePage/artifacts/BricksContainer.ts
@@ -1,10 +1,11 @@
 import { Brick } from './Brick';
 import { Ball } from './Ball';
+import { LEVELS } from '../utils/levels';
 
 export class BricksContainer {
-  private readonly _rowsAmount = 4;
+  private readonly _rowsAmount: number = 0;
 
-  private readonly _columnsAmount: number;
+  private readonly _columnsAmount: number = 0;
 
   private readonly _bricksGap = 5;
 
@@ -13,16 +14,25 @@ export class BricksContainer {
   constructor(
     private readonly ctx: CanvasRenderingContext2D,
     private readonly canvasWidth: number,
+    private readonly level: number,
     private readonly ball: Ball,
     private readonly increaseScore: () => void,
+    private readonly increaseLevel: () => void,
   ) {
-    this._columnsAmount = Math.trunc(canvasWidth / (Brick.width + this._bricksGap));
+    const currentLevel = LEVELS[level];
 
-    for (let i = 0; i < this._columnsAmount; i++) {
-      this._bricksMatrix[i] = [];
+    if (Array.isArray(currentLevel) && Array.isArray(currentLevel[0])) {
+      this._rowsAmount = currentLevel.length;
+      this._columnsAmount = currentLevel[0].length;
 
-      for (let j = 0; j < this._rowsAmount; j++) {
-        this._bricksMatrix[i][j] = new Brick(this.ctx, ball, 0, 0);
+      for (let i = 0; i < this._columnsAmount; i++) {
+        this._bricksMatrix[i] = [];
+
+        for (let j = 0; j < this._rowsAmount; j++) {
+          const isActiveBrick = Array.isArray(currentLevel[j]) ? currentLevel[j][i] : true;
+
+          this._bricksMatrix[i][j] = new Brick(this.ctx, ball, isActiveBrick, 0, 0);
+        }
       }
     }
   }
@@ -44,6 +54,7 @@ export class BricksContainer {
     const bricksTotalWidth = columnsAmount * (Brick.width + bricksGap) - bricksGap;
     const leftOffset = (canvasWidth - bricksTotalWidth) / 2;
     const topOffset = 55;
+    let activeBricksCounter = 0;
 
     for (let i = 0; i < this._bricksMatrix.length; i++) {
       for (let j = 0; j < this._bricksMatrix[i].length; j++) {
@@ -55,11 +66,18 @@ export class BricksContainer {
         if (brick.isActive) {
           brick.draw();
           const hasCollision = brick.isDetectedCollision();
+
           if (hasCollision) {
             this.increaseScore();
+          } else {
+            activeBricksCounter++;
           }
         }
       }
+    }
+
+    if (activeBricksCounter === 0) {
+      this.increaseLevel();
     }
   }
 }
