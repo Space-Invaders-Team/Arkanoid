2e029d97ee55bcc2372ea7718d3e05fe6f4dafb0
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2023-03-16 11:27:10 +0100

    App tests

--- packages/client/src/components/App/App.test.tsx
+++ packages/client/src/components/App/App.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 // defineProperty for jest-test
 // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 import './matchMedia.mock'; // Must be imported before the tested file
 import '@testing-library/jest-dom/extend-expect';
 
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { AnyAction, configureStore, Store } from '@reduxjs/toolkit';
 
 import { App } from './App';
 import { rootReducer } from '../../store/store';
@@ -14,11 +14,24 @@ import { rootReducer } from '../../store/store';
 global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }));
 
 describe('App component tests', () => {
+  let store: Store<unknown, AnyAction>;
+  beforeEach(() => {
+    store = configureStore({ reducer: rootReducer });
+  });
+
   test('Test the loader is shown', async () => {
-    const store = configureStore({ reducer: rootReducer });
     render(<Provider store={store}><App /></Provider>);
 
     const component = await screen.findByTestId('loader');
     expect(component).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.getByTestId('loader'));
+  });
+
+  test('Test the navigation is shown', async () => {
+    render(<Provider store={store}><App /></Provider>);
+
+    const component = await screen.findByTestId('navigation');
+    expect(component).toBeInTheDocument();
+    expect(component).toMatchSnapshot();
   });
 });
