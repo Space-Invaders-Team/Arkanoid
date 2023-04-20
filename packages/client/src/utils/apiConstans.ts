1a40ab46c0af55ff2ecaac8826957801728f8fab
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2023-04-20 14:38:01 +0300

    feat(task-51): add deployment to yandex-cloud

--- packages/client/src/utils/apiConstans.ts
+++ packages/client/src/utils/apiConstans.ts
@@ -1,5 +1,5 @@
 export const BASE_URL_YANDEX = 'https://ya-praktikum.tech/api/v2';
 export const LEADERBOARD = '/leaderboard';
 export const TEAM_NAME = 'spaceInvadersTeam';
-export const BASE_URL_API = 'http://localhost:3001/api/v1';
+export const BASE_URL_API = `http://${import.meta.env?.VITE_APP_HOSTNAME}/api/v1`;
 export const FORUM = '/forums';
